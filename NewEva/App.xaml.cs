c5f418e2d4e03cc48381464f2b51cb3f59144b02
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-08-25 22:26:14 +0700

    123

--- NewEva/App.xaml.cs
+++ NewEva/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -18,6 +19,30 @@ namespace NewEva
         {
             //FileDelete.FileDel(); //удаление файлов
         }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+
+            window.Closed += OnClosed;
+            
+
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            
+            FileDelete.FileDel(); //удаление файлов
+            Current.Shutdown();
+            // Shut things down 10 secs from now
+            //Timer t = new Timer(
+            //    (state) => { App.Current.Shutdown(); },
+            //    null, 10000, -1);
+        }
+
+
 
         //protected override void OnClosed(EventArgs e)
         //{
