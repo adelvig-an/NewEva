80dae28eca889add376c152d694714100cd7327a
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-09-08 16:20:59 +0700

    123

---
+++ NewEva/VM/Customer/CustomerVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewEva.VM.Customer
+{
+    public class CustomerVM : ViewModelBase
+    {
+        private PageVM currentPage;
+        public PageVM CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                CurrentPage?.Write();
+                SetProperty(ref currentPage, value);
+            }
+        }
+
+        public CustomerVM()
+        {
+
+        }
+    }
+}
