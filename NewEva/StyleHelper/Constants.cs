5f772fd210f33b8ed40e2f9be04680cdd76a9e8d
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-11-02 02:27:55 +0700

    New Style TextBox!

--- NewEva/StyleHelper/Constants.cs
+++ NewEva/StyleHelper/Constants.cs
@@ -4,8 +4,8 @@ namespace NewEva.StyleHelper
 {
     public static class Constants
     {
-        public static readonly Thickness TextBoxDefaultPadding = new Thickness(0, 4, 0, 4);
-        public static readonly Thickness TextBoxFloatingHintPadding = new Thickness(0, 12, 0, 0);
+        public static readonly Thickness TextBoxDefaultPadding = new Thickness(8, 4, 8, 4);
+        public static readonly Thickness TextBoxFloatingHintPadding = new Thickness(8, 12, 8, 0);
         public static readonly Thickness DefaultTextBoxViewMargin = new Thickness(1, 0, 1, 0);
         public static readonly Thickness DefaultTextBoxViewMarginEmbedded = new Thickness(0);
         public const double TextBoxNotEnabledOpacity = 0.56;
