ab72efaaddee368db6f24d5e4409eefd06b9bb13
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-12-06 19:45:30 +0700

    Рзделение на проекты

--- 10Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace _10Model
-{
-    public class Class1
-    {
-    }
-}
--- 20DbLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace _20DbLayer
-{
-    public class Class1
-    {
-    }
-}
